f5d707bf9c3def1be4fc8e36df19b34f1ba0f253
Tyler Getsay <tylergetsay@gmail.com>
2023-01-30 08:32:15 -0500

    chore: add homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,44 @@
-    <template>
-      <div class="flex flex-col gap-2" v-if="!pending">
-        <div>
-          <div class="flex  gap-4 mb-2 justify-between">
-            <div class="flex items-center gap-4">
-              <label for="search">Search</label>
-              <input type="text" class="border border-black" v-model="search">
-            </div>
-            <div>
-              {{filteredApps.length}} / {{data.length}}
-            </div>
-          </div>
-        </div>
-        <div v-for="app in filteredApps">
-          <nuxt-link :to="'/app/' + app.id">
-            <div class="flex items-center gap-2">
-              <img :src="app.icon" class="w-5 h-5">
-              <div>
-                {{app.name}} - <span class="text-gray-400">{{app.containers[0].image}}</span>
-              </div>
-            </div>
-          </nuxt-link>
-        </div>
-      </div>
-    </template>
-    <script setup>
-    const { data, pending } = await useAsyncData(() => queryContent('/apps').find());
+<template>
+  <div class="flex justify-center mt-4 mb-6 flex-col items-center gap-4">
+    <h1 class="text-3xl">Self Hosted App Metadata</h1>
+    <p>The purpose of this project is to maintain a <a href="https://github.com/tylergets/selfhosted-apps-meta/blob/main/meta/interfaces/interfaces.d.ts#L38" class="link">consistent schema</a> for many self hosted apps.</p>
+    <p>It can be used for creating "app stores", alternative configurations, etc.</p>
+  </div>
+  <div class="flex justify-center gap-4">
+    <nuxt-link to="/apps" class="btn">View All Apps</nuxt-link>
+    <a href="https://github.com/tylergets/selfhosted-apps-meta" target="_blank" class="btn">View Github</a>
+  </div>
+  <div>
+    <div class="mt-4 mb-2">
+      <h2 class="text-xl">FAQ</h2>
+    </div>
+    <div v-for="question in faq" class="my-4">
+      <div class="font-bold">{{question.question}}</div>
+      <div>{{question.answer}}</div>
+    </div>
+  </div>
+</template>
 
-    const search = ref('');
+<script>
+export default {
+  name: "index",
+  data() {
+    return {
+      faq: [
+        {
+          question: 'Why not just use docker-compose?',
+          answer: 'Docker-compose is perfect for running applications, but its schema is missing information necessary to fully run the application. This includes information required for reverse proxies, dashboards, database setup, etc. The good thing is our schema can be used to generate docker-compose files.'
+        },
+        {
+          question: 'How to use it?',
+          answer: 'If you are a developer, get involved with the project on Github. If you are a user, you can browse the data to create a configuration for your own server.'
+        }
+      ]
+    }
+  }
+}
+</script>
 
-    const filteredApps = computed(() => {
-      if (!search.value) {
-        return data.value;
-      }
+<style scoped>
 
-      return data.value.filter((app) => {
-        return app.name.toLowerCase().includes(search.value.toLowerCase());
-      });
-    });
-
-    </script>
+</style>
